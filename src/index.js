41b3128ca4729c37e95ca8adcb25b70dc9306e6b
schvbertt <diamenttx@gmail.com>
2020-04-25 13:18:28 +0200

    fixed redirects

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {RoomProvider} from './context'
 
 ReactDOM.render(
   <RoomProvider>
-    <Router>
+    <Router basename='react-apartments'>
       <React.StrictMode>
         <App />
       </React.StrictMode>
